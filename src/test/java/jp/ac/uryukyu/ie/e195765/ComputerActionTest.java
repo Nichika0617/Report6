4f2d16600583100de9f8379a59ee19c5171a8b05
Yuge Nichika <e195765@ie.u-ryukyu.ac.jp>
2020-01-07 09:42:11 +0900

    change ComputerActionTest & add ComputerActionReturn30

--- src/test/java/jp/ac/uryukyu/ie/e195765/ComputerActionTest.java
+++ src/test/java/jp/ac/uryukyu/ie/e195765/ComputerActionTest.java
@@ -9,7 +9,7 @@ class ComputerActionTest {
      * 返答可能な3つの数字の中に30も含まれる場合、30と言って自滅せずに29を返しているか確認するテスト。
      */
     @Test
-    void computerAction() {
+    void computerActionReturn29() {
         ComputerAction cpu26 = new ComputerAction(26);
         assertEquals(29,cpu26.ComputerAction(26));//26で回ってきた時
         ComputerAction cpu27 = new ComputerAction(27);
@@ -20,5 +20,21 @@ class ComputerActionTest {
         assertEquals(29,cpu26.ComputerAction(26));
         assertEquals(29,cpu27.ComputerAction(27));
         assertEquals(29,cpu28.ComputerAction(28));
+
+
     }
-}
+
+    /**
+     * 返答可能な最小の数字が30の場合、31や32は返さずに30を返して負けているかを確認するテスト
+     */
+    @Test
+    void computerActionReturn30(){
+        ComputerAction cpu29 = new ComputerAction(29);
+        assertEquals(30,cpu29.ComputerAction(30)); //29を渡された時に31や32ではなく、しっかりと30を返しているか確認するテスト
+        //乱数で奇跡的に30を返している可能性もあるので、複数回実行する。
+        assertEquals(30,cpu29.ComputerAction(30));
+        assertEquals(30,cpu29.ComputerAction(30));
+        assertEquals(30,cpu29.ComputerAction(30));
+    }
+
+    }
